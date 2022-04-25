81dfd2e130b50a664fe2316009ffa1f888db4bc7
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-04-25 09:25:46 -0500

    html generate

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length)
+const fs = require('fs')
+const generatePage = require('./src/page-template')
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileInfo => console.log(profileInfo))
-}
+const profileDataArgs = process.argv.slice(2)
 
-printProfileData(profileDataArgs)
+const [name, github] = profileDataArgs
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    
+    if (err) throw err;
+
+    console.log("Portfolio complete! Check out index.html to see the output!")
+})
